63c375635a9cf114c54feb94e9a060f1a949a248
Ahmad Zaeni 6706174120 <43094463+ahmadzaen@users.noreply.github.com>
2019-10-31 17:44:18 +0700

    launch Daily Calorie Needs

---
+++ app/src/main/java/com/ahmadzaen/dailycalorieneeds/MainActivity.java
@@ -0,0 +1,25 @@
+package com.ahmadzaen.dailycalorieneeds;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
